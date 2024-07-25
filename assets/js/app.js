7ff08b48cffbb086607b451b6c280664a9178a7d
zotozen <soto.davidj@outlook.com>
2024-07-24 17:38:04 -0700

    nightly edit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,37 @@
-document.querySelector('.menu-toggle').addEventListener('click', function(e) {
-    e.preventDefault();
-    document.querySelector('.nav-links').classList.toggle('active');
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('contactForm');
+
+    document.querySelector('.menu-toggle').addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector('.nav-links').classList.toggle('active');
+    });
+    
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        // Perform the actual form submission
+        fetch(form.action, {
+            method: form.method,
+            body: new FormData(form),
+            headers: {
+                'Accept': 'application/json'
+            }
+        }).then(response => {
+            if (response.ok) {
+                alert('Message sent!');
+                form.reset();
+            } else {
+                response.json().then(data => {
+                    if (Object.hasOwn(data, 'errors')) {
+                        alert(data["errors"].map(error => error["message"]).join(", "));
+                    } else {
+                        alert('Oops! There was a problem submitting your form');
+                    }
+                })
+            }
+        }).catch(error => {
+            alert('Oops! There was a problem submitting your form');
+        });
+    });
 });
+
